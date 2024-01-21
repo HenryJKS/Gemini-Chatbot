d13008ddb54f1994a8ef616679450f11864952e4
HenryJKS <henryjooji30@gmail.com>
2024-01-21 19:43:17 -0300

    release 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const [text, setText] = useState("");
@@ -13,42 +16,76 @@ const App = () => {
     const data = await response.json();
     console.log(data);
 
-    setMessages([
-      ...messages,
-      {
-        author: data.messages[0].content,
-        bot: data.candidates[0].content,
-      },
-    ]);
+    if (
+      data.candidates &&
+      data.candidates.length > 0 &&
+      data.candidates[0].content
+    ) {
+      setMessages([
+        ...messages,
+        {
+          author: data.messages[0].content,
+          bot: data.candidates[0].content,
+        },
+      ]);
+    } else {
+      setMessages([
+        ...messages,
+        {
+          author: data.messages[0].content,
+          bot: "Sorry, I don't understand.",
+        },
+      ]);
+    }
   };
 
-  console.log(text);
-
   return (
-    <div className="chatbot">
-      <div className="chat-header">
-        <div className="info-container">
-          <h3>Chat with</h3>
-          <h2>Gemini Bot</h2>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path
-            fill="rgb(6, 120, 84)"
-            fillOpacity="1"
-            d="M0,224L60,218.7C120,213,240,203,360,186.7C480,171,600,149,720,154.7C840,160,960,192,1080,186.7C1200,181,1320,139,1380,117.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-          ></path>
-        </svg>
-      </div>
-      <div className="feed">
-        {messages?.map((message, _index) => (
-          <div key={_index}>
-            <div className="question bubble">{message.author}</div>
-            <div className="response bubble">{message.bot}</div>
-          </div>
-        ))}
-      </div>
-      <textarea value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={getResponse}>➔</button>
+    // <div className="chatbot">
+    //   <div className="chat-header">
+    //     <div className="info-container">
+    //       <h3>Chat with</h3>
+    //       <h2>Gemini Bot</h2>
+    //     </div>
+    //   </div>
+    //   <div className="feed">
+    //     {messages?.map((message, _index) => (
+    //       <div key={_index}>
+    //         <div className="question bubble">{message.author}</div>
+    //         <div className="response bubble">{message.bot}</div>
+    //       </div>
+    //     ))}
+    //   </div>
+    //   <textarea value={text} onChange={(e) => setText(e.target.value)} />
+    //   <button onClick={getResponse}>➔</button>
+    // </div>
+
+    <div className="d-flex justify-content-center align-items-center" style={{height: "100vh"}}>
+      <Card className="border rounded border-primary w-50">
+        <Card.Header className="cardheader text-center">GeminiBot</Card.Header>
+        <Card.Body>
+          <Card.Text className="cardtext">
+            {messages?.map((message, _index) => (
+              <div key={_index}>
+                <div className="question bubble">{message.author}</div>
+                <div className="response bubble">{message.bot}</div>
+              </div>
+            ))}
+          </Card.Text>
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="form-control"
+            rows="3"
+          />
+          <Button
+            onClick={getResponse}
+            className="btn btn-primary mt-3"
+            style={{ width: "100%" }}
+          >
+            Send
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
