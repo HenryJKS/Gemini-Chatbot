799fc4653031f55d082c847b5a28ff3407d3ae2e
HenryJKS <henryjooji30@gmail.com>
2024-01-22 14:16:51 -0300

    release 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Card from "react-bootstrap/Card";
+import { Spinner } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // Send a GET request to the backend
   const getResponse = async () => {
+    setLoading(true);
     const response = await fetch(`http://localhost:8000/prompt/${text}`);
 
     const data = await response.json();
@@ -33,21 +36,24 @@ const App = () => {
         },
       ]);
     }
+
+    text && setText("");
+    setLoading(false);
   };
 
   return (
     <div className="principal d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
-      <Card className="w-50" style={{background: 'rgba(255, 255, 255, 0.15)', borderRadius:'16px',boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)', backdropFilter: 'blur(2px)', borderColor: 'black', WebkitBackdropFilter: 'blur(3.8px)'}}>
-        <Card.Header className="cardheader text-center" style={{color: '#fff'}}>GeminiBot</Card.Header>
+      <Card className="w-50" style={{ background: 'rgba(255, 255, 255, 0.15)', borderRadius: '16px', boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)', backdropFilter: 'blur(2px)', borderColor: 'black', WebkitBackdropFilter: 'blur(3.8px)' }}>
+        <Card.Header className="cardheader text-center" style={{ color: '#fff' }}>GeminiBot</Card.Header>
         <Card.Body>
           <Card.Text className="cardtext" style={{ maxHeight: '200px', overflowY: 'auto' }}>
             {messages?.map((message, _index) => (
               <div key={_index}>
-                <div style={{ width: '100%', textAlign: 'left'}}>
-                  <div className="question bubble border rounded border-success" style={{ display: 'inline-block', margin: "1%", padding: "1%", color: "#fff"}}>User: {message.author}</div>
+                <div style={{ width: '100%', textAlign: 'left' }}>
+                  <div className="question bubble border rounded border-success" style={{ display: 'inline-block', margin: "1%", padding: "1%", color: "#fff" }}>User: {message.author}</div>
                 </div>
                 <div style={{ width: '100%', textAlign: 'right' }}>
-                  <div className="response bubble border rounded border-info" style={{ display: 'inline-block', margin: "1%", padding: "1%", color: "#fff"}}>GeminiBot: {message.bot}</div>
+                  <div className="response bubble border rounded border-info" style={{ display: 'inline-block', margin: "1%", padding: "1%", color: "#fff" }}>GeminiBot: {message.bot}</div>
                 </div>
               </div>
             ))}
@@ -60,10 +66,10 @@ const App = () => {
           />
           <button
             onClick={getResponse}
-            disabled={!text}
+            disabled={!text || loading} // Desabilite o botão se não houver texto ou se estiver carregando
             className="mt-3"
-            style={{margin: "auto", justifyContent: "center", justifyItems: "center", display: "flex"}}>
-            <span>➛ Send</span>
+            style={{ margin: "auto", justifyContent: "center", justifyItems: "center", display: "flex" }}>
+            {loading ? (<Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />) : (<span>➛ Send</span>)}
           </button>
         </Card.Body>
       </Card>
